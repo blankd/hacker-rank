5bc1ffc27f1eef4184b36bc7dffb51347001da30
blankd <darek.blankenbuhler>
2018-05-24 18:33:57 -0700

    Completed No Idea Sets Challenge and proper coding updates

---
+++ python/src/blankd/hackerrank/python/sets/no_idea.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    with open("path/to/data/file") as f:
+        total = 0
+        lines = f.readlines()
+        (m, n) = lines[0].split()
+        nums = lines[1].split()
+        like = set(lines[2].split())
+        dislike = set(lines[3].split())
+
+        for num in nums:
+            if num in like:
+                total += 1
+            if num in dislike:
+                total -= 1
+
+        print(total)
