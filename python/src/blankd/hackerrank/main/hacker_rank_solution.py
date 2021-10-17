9b4f2cc4f3efcca862be8849d3ed898d6d5d2948
Darek Blankenbuhler <darek.blankenbuhler@gmail.com>
2021-10-17 08:12:12 -0700

    Added python hacker rank solutions file

---
+++ python/src/blankd/hackerrank/main/hacker_rank_solution.py
@@ -0,0 +1,13 @@
+class HackerRankSolution:
+    def prepare_data(self, file_to_rade):
+        raise NotImplemented("prepare_data has not been implemented")
+
+    def run_solution(self):
+        raise NotImplemented("run_solution has not been implemented.")
+
+    def print_results(self):
+        raise NotImplemented("print_results has not been implemented.")
+
+
+if __name__ == '__main__':
+    print("Nothing to urn")
